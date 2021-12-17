1873cd91ef2bfa7237d68d18b5939a19fa441af4
Andrew Kowalczyk <andrewrkowalczyk@gmail.com>
2021-12-16 16:55:53 -0800

    add solution for day 8, part 1

---
+++ 2021/day08/part1.js
@@ -0,0 +1,22 @@
+const path = require("path");
+const fs = require("fs");
+const data = fs.readFileSync(path.join(__dirname, "input.txt"), "utf8");
+const input = data.trim();
+
+const rows = input.split(/\n/).map((r) => r.split(" | "));
+
+console.log(
+  rows
+    .map(([_, output]) => output.split(" "))
+    .reduce((acc, cur) => [...acc, ...cur], [])
+    .reduce(
+      (acc, cur) =>
+        cur.length === 2 ||
+        cur.length === 3 ||
+        cur.length === 4 ||
+        cur.length === 7
+          ? acc + 1
+          : acc,
+      0
+    )
+);
